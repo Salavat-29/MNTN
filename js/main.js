87f9f7eb864beac8c4756c34c69ff29c8b0f70fb
Salavat-29 <galimov.sg97@gmail.com>
2023-06-12 13:52:54 +0300

    second-to-last save

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const navBtn = document.querySelector('.nav-btn-media')
+const mobileNav = document.querySelector('.mobile-nav')
+const body = document.body
+
+// Клик по кнопке
+navBtn.addEventListener('click', function (event) {
+    event.stopPropagation()
+    toggleMobileNav()
+})
+
+// Клик по окну за пределами навигации
+window.addEventListener('click', function() {
+    if (body.classList.contains('no-scroll')) {
+        toggleMobileNav()
+    }
+} )
+
+// Останавливаем клик по открытой мобильной навигации
+mobileNav.addEventListener('click', function (event) {
+    event.stopPropagation()
+})
+
+function toggleMobileNav() {
+    mobileNav.classList.toggle('mobile-nav-active')
+    navBtn.classList.toggle('nav-btn-close')
+    body.classList.toggle('no-scroll')
+}
+
